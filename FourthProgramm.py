e13bc720393df3ce6fb3062d44b8a0612b10b23b
areejatif885 <areejatif885@gmail.com>
2026-07-19 06:04:38 -0700

    fourth commit

---
+++ FourthProgramm.py
@@ -0,0 +1,13 @@
+movie1=input("Enter first movie") 
+
+movie2=input("Enter second movie")
+
+movie3=input("Enter third movie")
+
+movies=[]
+
+movies.append(movie1)
+movies.append(movie2)
+movies.append(movie3)
+
+print(movies)
