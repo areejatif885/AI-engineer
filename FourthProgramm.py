30309a3b0517a6bd844e7faeae6e7fff2601115b
areejatif885 <areejatif885@gmail.com>
2026-07-19 07:14:32 -0700

    fifthh commit

--- FourthProgramm.py
+++ FourthProgramm.py
@@ -1,24 +1,32 @@
-movie1=input("Enter first movie") 
+# movie1=input("Enter first movie") 
 
-movie2=input("Enter second movie")
+# movie2=input("Enter second movie")
 
-movie3=input("Enter third movie")
+# movie3=input("Enter third movie")
 
-movies=[]
+# movies=[]
 
-movies.append(movie1)
-movies.append(movie2)
-movies.append(movie3)
+# movies.append(movie1)
+# movies.append(movie2)
+# movies.append(movie3)
 
-print(movies)
+# print(movies)
 
-List=input("Enter numbers : ")
-List1=[]
-List1.append(list)
-Palindrome_1 = list1.copy()
-Palindeome_2 = Palindrome_1.reverse()
+# List=input("Enter numbers : ")
+# List1=[]
+# List1.append(List)
+# Palindrome_1 = List1.copy()
+# Palindeome_2 = Palindrome_1.reverse()
 
-if(Palindrome_1 == Palindeome_2):
-    print("It is palindrome")
-else:
-    print("Not Palindrome")
+# if(Palindrome_1 == Palindeome_2):
+#     print("It is palindrome")
+# else:
+#     print("Not Palindrome")
+
+
+# tuple=("c","b", "a" ,"a" ,"b" , "b" ,"a" , "c", "b", "a","c", "a", "h", "e", "a", "r", "t", "a")
+# print(tuple.count("a"))
+
+list_1 =["c","b", "a" ,"a" ,"b" , "b" ,"a" , "c", "b", "a","c", "a", "h", "e", "a", "r", "t", "a"]
+list_1.sort()
+print(list_1)
